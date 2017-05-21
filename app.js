e0e69d488f5706afb0d0f29f23302ebc96eb620d
Saroj Shrestha <sarojshresthaa@yahoo.com>
2017-05-21 06:06:59 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine","ejs");
+
+app.get('/', function(req, res){
+    res.render("landing");
+})
+
+app.get('/cities', function(req,res){
+    var cities = [
+        {name:'Tokyo',image:'https://en.wikipedia.org/wiki/Tokyo#/media/File:TokyoMetropolitanGovernmentOffice.jpg'},
+        {name:'Osaka',image:'https://en.wikipedia.org/wiki/Osaka#/media/File:Osaka_Castle_02bs3200.jpg'},
+        {name:'Nagoya',image:'https://en.wikipedia.org/wiki/Port_of_Nagoya#/media/File:Nagoya_Port_02.jpg'},
+        {name:'Kyoto',image:'https://en.wikipedia.org/wiki/Kyoto#/media/File:Atago07.JPG'}
+        ];
+    res.render('city.ejs');
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Blog server is running');
+});
