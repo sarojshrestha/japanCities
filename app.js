dc715c9e253fc57f7aa1381ad58e2c87534b457a
Saroj Shrestha <sarojshresthaa@yahoo.com>
2017-06-03 08:33:19 +0000

    update pages with data

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.set("view engine","ejs");
 
 const citySchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    desc: String
 });
 
 const City = mongoose.model('City',citySchema);
@@ -28,7 +29,7 @@ const City = mongoose.model('City',citySchema);
 */
 
 app.get('/', function(req, res){
-    res.render("landing");
+    res.render("Index");
 })
 
 app.get('/cities', function(req,res){
@@ -44,14 +45,11 @@ app.get('/cities', function(req,res){
 app.post('/cities', function(req,res){
     var name = req.body.city;
     var image = req.body.image;
-    var addCity= {name:name,image:image}
+    var desc = req.body.desc;
+    var addCity= {name:name,image:image,desc:desc};
     console.log(addCity);
     //cities.push(addCity)
-   City.create(
-       {
-           name:name,
-           image:image
-       }, function(err,data){
+   City.create(addCity, function(err,data){
     if(err){
         console.log(err)
     }else{
@@ -64,6 +62,17 @@ app.post('/cities', function(req,res){
 app.get('/cities/new', function(req,res){
     console.log('new');
    res.render('new'); 
+});
+
+app.get('/city/:id', function(req, res){
+    const id = req.params.id;  
+    City.findById(id,function(err, foundData){
+        if(err){
+            console.log(err)
+        }else{
+             res.render('show',{city:foundData});
+        }
+    });
 });
 
 app.listen(PORT, process.env.IP, function(){
